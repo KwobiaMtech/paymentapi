bf2430e73485d0154a26fa736ea408e9f1d33d19
kwobia-at-303179111604 <kwobia@303179111604>
2021-04-16 17:55:22 +0000

    fixed allowed hosts issue

--- paymentapi/core/settings.py
+++ paymentapi/core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-(kq6e%6_p-7-j^1yd&s*q$4uw29##tqpr0z!-mlfg2xcpwdp=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split(" ")
 
 # Application definition
 
